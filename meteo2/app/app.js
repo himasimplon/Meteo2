2a82931bd8ca2669b00c729cd3851ecaa095140e
Mathéo <matheo.zeller@gmail.com>
2017-12-28 09:54:42 +0100

    Projet final

--- meteo2/app/app.js
+++ meteo2/app/app.js
@@ -1,70 +1,33 @@
-// $(document).ready(function(){
-
-// 	$("#submitWeather").click(function(){
-// 		var city = $("#city").val();
-// 		console.log(city);
-// 		// if(city != ''){
-// 			$.ajax({
-// 				url: "http://api.openweathermap.org/data/2.5/weather?lat=44.5&units=metric&lon=2.58&APPID=6b0093ca3ed371f163ae90b3957b8b98", dataType: 'json',
-// 				succes: function(data){
-// 					console.log(data);
-// 				},
-// 				error: function(){
-// 					console.log("error");
-// 				}
-// 			});
-// 		// }else{
-// 			//$("#error").html('Field cannot be empty')
-// 		// }
-// 	});
-
-// });
-
-
-
 (function main(){
-	var xhr=createXHR();	
+	$('#error').hide();
 	$('#submitWeather').on('click',function(){
-	//getJson();
-	getJqAjax();
-})
+		getJqAjax();
+	})
 function getJqAjax(){
 	var city = $('#city').val();
+
+
 	if(city != ''){
 		$.ajax({
-			url: 'http://api.openweathermap.org/data/2.5/weather?q='+city+"&units=metric"+"&lang=fr"+"&APPID=fa23699725f4880f535416fdefe45b42",
+			url: 'http://api.openweathermap.org/data/2.5/weather?q='+city+"&units=metric"+"&appid=5fe735476d87c79b7f242f8f480d72a0",
 			datatype:'json',
 			success: function(data){
-				$("#error").html('');
-				console.log("success");
-				console.log(data)
-			}, error : function(){
-				$("#error").html('City not listed');
+				var widget = show(data);
+				$("#show").html(widget);
+				$('#city').val('');
+				$('#error').hide();
 			}
 		});
 	}else{
-		$("#error").html('Field cannot be empty');
+		$("#error").show();
 	}
-}
+}	
+	function show(data){
+		return "<div>Ciel : "+data.weather[0].description+"</div>" +
+			   "<div>Humiditée : "+data.main.humidity+"</div>" +
+			   "<div>Température min. : "+data.main.temp_min+"</div>" +
+			   "<div>Température max. : "+data.main.temp_max+"</div>"
+	};
 
-function createXHR(){
 
-    var request = false;
-        try{
-            request = new ActiveXObject('Msxml2.XMLHTTP');
-        }catch(err2){
-            
-            try{
-                request = new ActiveXObject('Microsoft.XMLHTTP');
-            }catch(err3){
-				
-				try{
-					request = new XMLHttpRequest();
-				}catch(err1){
-					request = false;
-				}
-            }
-        }
-    return request;
-}
 })();
