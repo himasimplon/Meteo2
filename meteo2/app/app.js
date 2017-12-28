112f911742e6d66752e19ef2e4138d4bf0c48a2b
Pidgeey <ludwig.somo@gmail.com>
2017-12-28 15:18:49 +0100

    Ajout carte

--- meteo2/app/app.js
+++ meteo2/app/app.js
@@ -3,6 +3,8 @@
 	$('.submitWeather').on('click',function(){
 		getJqAjax();
 	})
+	var map = L.map('map').setView([46.318392, 2.975921], 8);
+	L.tileLayer('http://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png?{foo}', {foo: 'bar'}).addTo(map);
 function getJqAjax(){
 	var city = $('#city').val();
 
@@ -16,6 +18,8 @@ function getJqAjax(){
 				$("#show").html(widget);
 				$('#city').val('');
 				$('#error').hide();
+				console.log(data);
+				setmap(data.coord.lon,data.coord.lat);
 			},
 			error: function(){
 				$('#error').show();
@@ -26,11 +30,19 @@ function getJqAjax(){
 	}
 }	
 	function show(data){
-		return '<div class="info">Ciel : '+data.weather[0].description+'</div>' +
+		return '<div class="info">Coordonées : Lat '+data.coord.lat+', Lon '+data.coord.lon+'</div>'+
+			   '<div class="info">Ciel : '+data.weather[0].description+'</div>' +
+			   '<div class="info">Vent : '+data.wind.speed+' Km/h</div>'+
 			   '<div class="info">Humiditée : '+data.main.humidity+'</div>' +
+			   '<div class="info">Température. :'+data.main.temp+'</div>' +
 			   '<div class="info">Température min. : '+data.main.temp_min+'</div>' +
-			   '<div class="info">Température max. : '+data.main.temp_max+'</div>'
+			   '<div class="info">Température max. : '+data.main.temp_max+'</div>' 
+
 	};
+
+	function setmap(x,y){
+		map.setView([y, x],14)
+	}
 
 
 })();
