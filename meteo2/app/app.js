04c93c7d1667b3225f5f8b7f1a20dd280355ddac
Mathéo <matheo.zeller@gmail.com>
2017-12-28 11:34:22 +0100

    modification css

--- meteo2/app/app.js
+++ meteo2/app/app.js
@@ -1,6 +1,6 @@
 (function main(){
 	$('#error').hide();
-	$('#submitWeather').on('click',function(){
+	$('.submitWeather').on('click',function(){
 		getJqAjax();
 	})
 function getJqAjax(){
@@ -9,24 +9,27 @@ function getJqAjax(){
 
 	if(city != ''){
 		$.ajax({
-			url: 'http://api.openweathermap.org/data/2.5/weather?q='+city+"&units=metric"+"&appid=5fe735476d87c79b7f242f8f480d72a0",
+			url: 'http://api.openweathermap.org/data/2.5/weather?q='+city+"&lang=fr"+"&units=metric"+"&appid=5fe735476d87c79b7f242f8f480d72a0",
 			datatype:'json',
 			success: function(data){
 				var widget = show(data);
 				$("#show").html(widget);
 				$('#city').val('');
 				$('#error').hide();
-			}
+			},
+			error: function(){
+				$('#error').show();
+			},
 		});
 	}else{
 		$("#error").show();
 	}
 }	
 	function show(data){
-		return "<div>Ciel : "+data.weather[0].description+"</div>" +
-			   "<div>Humiditée : "+data.main.humidity+"</div>" +
-			   "<div>Température min. : "+data.main.temp_min+"</div>" +
-			   "<div>Température max. : "+data.main.temp_max+"</div>"
+		return '<div class="info">Ciel : '+data.weather[0].description+'</div>' +
+			   '<div class="info">Humiditée : '+data.main.humidity+'</div>' +
+			   '<div class="info">Température min. : '+data.main.temp_min+'</div>' +
+			   '<div class="info">Température max. : '+data.main.temp_max+'</div>'
 	};
 
 
